0be207eb184705874fc8c26a3467a6bea4b378e9
ushiboy <ushiboy.dev@gmail.com>
2015-12-18 23:17:40 +0900

    add html and web sever

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,28 @@ var path = require('path');
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 var del = require('del');
+var connect = require('connect');
+var connectLivereload = require('connect-livereload');
+var serveStatic = require('serve-static');
 
 gulp.task('clean', del.bind(null, [distDir]));
 
-gulp.task('dev', [], () => {
+gulp.task('html', () => {
+  return gulp.src('app/**/*.html')
+  .pipe(gulp.dest(distDir))
+  .pipe($.livereload());
+});
 
+gulp.task('serve', () => {
+  $.livereload.listen();
+  connect()
+  .use(connectLivereload())
+  .use(serveStatic(path.join(__dirname, distDir)))
+  .listen(3100);
+});
+
+gulp.task('dev', ['html', 'serve'], () => {
+  gulp.watch('app/**/*.html', ['html']);
 });
 
 gulp.task('default', ['clean'], () => {
